e2930cae2c3d65491425145483544876494bf6e0
Rottschaefer <rottschaefer54@gmail.com>
2023-10-18 07:55:11 -0300

    autenticação via JWT finalizada e incluída no final do cadastro. Início do trabalho com as expenses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,11 @@
 class User < ApplicationRecord
+
+    has_many :expenses
+
     has_secure_password
   
     validates :name, presence: true
-    validates :email, presence: true, uniqueness: true
-    validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
-    validates :password, presence: true, length: { minimum: 6 }
+    validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :password, length: { minimum: 6 }
   end
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_17_103841) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_18_103748) do
+  create_table "expenses", force: :cascade do |t|
+    t.string "name"
+    t.float "spent"
+    t.float "to_spend"
+    t.integer "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_expenses_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "name", null: false
     t.string "email", null: false
@@ -20,4 +30,5 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_17_103841) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "expenses", "users"
 end
