f755180b37dc2f9b76e88d24c11922f36f606530
Rottschaefer <rottschaefer54@gmail.com>
2024-01-02 08:58:02 -0300

    fixing migrations

--- db/migrate/20231231150120_remove_info_to_accounts.rb
+++ db/migrate/20231231150120_remove_info_to_accounts.rb
@@ -1,6 +1,6 @@
-class RemoveInfoToAccounts < ActiveRecord::Migration[7.1]
-  def change
-    # remove_column :accounts, :name, :string
-    remove_column :accounts, :transfer_number, :string
-  end
-end
+# class RemoveInfoToAccounts < ActiveRecord::Migration[7.1]
+#   def change
+#     # remove_column :accounts, :name, :string
+#     remove_column :accounts, :transfer_number, :string
+#   end
+# end
